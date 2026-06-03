ae4c69b74a855380e5887f7970c876e2f2a0f441
Mylibs-ridwan <tbhsmkn@gmail.com>
2026-06-03 21:14:37 +0700

    Update app_essay.py

--- app_essay.py
+++ app_essay.py
@@ -13,7 +13,7 @@ if current_dir not in sys.path:
     sys.path.append(current_dir)
 
 # Sekarang Anda bisa melakukan import tanpa error
-from dataset import EssayScoringModel, get_transforms_and_tokenizer
+from dataset import MultimodalEssayModel, get_transforms_and_tokenizer
 
 # ── 1. CONFIG & LOAD MODEL (DI-CACHE) ──────────────────────────────────
 @st.cache_resource
